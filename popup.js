663c84edc9aaeb8f47f9e6e7528deb755ed3f1d4
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-04-25 23:18:45 +0530

    added toggle switch

--- popup.js
+++ popup.js
@@ -9,3 +9,15 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     });
   });
   
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+  if (document.body.classList.contains('dark-mode')) {
+    toggleButton.textContent = '🌕 Light Mode';
+  } else {
+    toggleButton.textContent = '🌙 Dark Mode';
+  }
+});
+
