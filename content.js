f158386eac6540e3dc38077dd25f5251ad9fc9a5
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-04-29 23:54:29 +0530

    added js file

--- content.js
+++ content.js
@@ -1,39 +1,29 @@
-function extractSeoData() {
-    const title = document.title;
-    const description = document.querySelector('meta[name="description"]')
-      ? document.querySelector('meta[name="description"]').content
-      : "No description tag found";
-    const wordCount = document.body.innerText.split(" ").length;
-  
-    const headings = [];
-    for (let i = 1; i <= 6; i++) {
-      const headingTags = document.querySelectorAll(`h${i}`);
-      headingTags.forEach(tag => {
-        headings.push(tag.innerText);
-      });
-    }
-  
-    const internalLinks = [];
-    const allLinks = document.querySelectorAll("a");
-    allLinks.forEach(link => {
-      if (link.href && link.href.includes(window.location.hostname)) {
-        internalLinks.push(link.href);
-      }
-    });
+{
+  "manifest_version": 3,
+  "name": "SEO Analyzer",
+  "description": "Analyze the on-page SEO of any website",
+  "version": "1.0",
+  "permissions": ["activeTab"],
 
-    return {
-      title,
-      description,
-      wordCount,
-      headings,
-      internalLinks,
-    };
-  }
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getSeoData") {
-      const seoData = extractSeoData();
-      sendResponse(seoData);
+  "action": {
+    "default_popup": "popup.html",
+    "default_icon": {
+      "16": "seo.png",
+      "48": "seo.png",
+      "128": "seo.png"
     }
-  });
-  
+
+  },
+  "content_scripts": [
+    {
+      "matches": ["<all_urls>"],
+      "js": ["content.js"],
+      "run_at": "document_idle"
+    }
+  ],
+  "icons": {
+    "16": "seo.png",
+    "48": "seo.png",
+    "128": "seo.png"
+  }
+}
